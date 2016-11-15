834e01d1706f25334e7da362fb369a42d5a3a54f
taoyuhong <taoyuhong@163.com>
2016-11-15 20:54:30 +0800

    add some examples.

--- cmds/do_test.c
+++ cmds/do_test.c
@@ -1,11 +1,41 @@
+#include <stdlib.h>
 #include <init.h>
 #include <input_cmd.h>
 #include <log_project3.h>
 
+static char *seq[] = {
+	"run example/0_start.txt",
+	"run example/1_load_r.txt",
+	"run example/2_load_l.txt",
+};
+
+#define NTEST	(sizeof(seq)/sizeof(char *))
+
 static int do_test(int argc, char *argv[])
 {
-	run_cmd("run example/test_00.txt");
-	run_cmd("run example/test_01.txt");
+	int i, err;
+	int index;
+
+	if (argc == 1) { 
+		for (i = 0; i < NTEST; i++) {
+			err = run_cmd(seq[i]);
+			if (err)
+				return err;
+		}
+		return 0;
+	}
+		
+	for (i = 1; i < argc; i++) {
+		index = atoi(argv[i]);
+		if (index < 0 || index >= NTEST) {
+			log_info("test err: %d outof range[0->%d]\n",
+				index, NTEST);
+			return -1;
+		}
+		err = run_cmd(seq[index]);
+		if (err)
+			return err;
+	}
 
 	return 0;
 }
